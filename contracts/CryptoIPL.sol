b97e8e8fe130610735bbc6af0f0173c7dbbe87af
Arjun Dev <arjun@Arjuns-MacBook-Pro.local>
2022-04-04 00:22:27 +0530

    feat: added smart contract

--- contracts/CryptoIPL.sol
+++ contracts/CryptoIPL.sol
@@ -36,4 +36,31 @@ contract MindGames {
   function lockGame() public onlyOwner {
     games[numOfGames].isAcceptingBets = false;
   }
+
+  function acceptBet(uint _gameNum, uint _teamNum) public payable{
+    require(_gameNum>0,"Wrong game selected");
+    require(games[_gameNum].isAcceptingBets == true, "Not accepting bets anymore");
+    require(msg.value == 1 ether, "Not the right amount");
+    if (_teamNum==1) {
+      games[_gameNum].team1bets++;
+    }
+    else if (_teamNum==2) {
+      games[_gameNum].team2bets++;
+    }
+  }
+
+  function closeGame(uint _gameNum, uint _teamNum) public onlyOwner{
+    require(_gameNum>0,"Wrong game selected");
+    require(games[numOfGames].isAcceptingBets == false, "Wrong game selected");
+    uint i;
+    if (_teamNum==1) {
+      for(i=1;i<=games[numOfGames].team1bets;i++){
+        transfer()
+      }
+    }
+    else if (_teamNum==2) {
+      games[_gameNum].team2bets++;
+    }
+
+  }
 }
